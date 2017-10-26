24e231ba7acb00c56821480e915a908133cf3a97
hongdp <hongdp@gmail.com>
2017-10-26 02:13:00 -0700

    Add keyboard input.

--- run.py
+++ run.py
@@ -1,17 +1,42 @@
 import gym
 import ppaquette_gym_super_mario
+from pynput import keyboard
 # import gym_pull
-# gym_pull.pull('github.com/ppaquette/gym-super-mario') 
+# gym_pull.pull('github.com/ppaquette/gym-super-mario')
 env = gym.make('ppaquette/SuperMarioBros-1-1-v0')
 env.reset()
 total_score = 0
-first = True
-while total_score < 32000:
-    action = [0,0,0,1,0,0]
-    obs, reward, is_finished, info = env.step(action)
-    if first:
-        first = False
-        print obs
-    env.render()
-    total_score = info["distance"]
-    print total_score
+action = [0,0,0,0,0,0]
+def on_press(key):
+    try:
+       if key.char == 'a':
+           action[1] = 1
+       elif key.char == 'd':
+           action[3] = 1
+       elif key.char == 'j':
+           action[4] = 1
+       elif key.char == 'k':
+           action[5] = 1
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+def on_release(key):
+    if key.char == 'a':
+        action[1] = 0
+    elif key.char == 'd':
+        action[3] = 0
+    elif key.char == 'j':
+        action[4] = 0
+    elif key.char == 'k':
+        action[5] = 0
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    while total_score < 32000:
+        obs, reward, is_finished, info = env.step(action)
+        env.render()
+        total_score = info["distance"]
+    listener.join()
