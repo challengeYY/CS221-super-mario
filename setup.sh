46a23a8c4e50bf007a3c27ea62431662d0e2c101
Yaqi Zhang <hsbzyq@gmail.com>
2017-12-15 01:53:04 -0800

    fixes to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-apt-get install scons libsdl1.2-dev subversion libgtk2.0-dev xvfb liblua
-cd fceux-2.2.2
-sudo scons install
+sudo apt-get install fceux
 
 virtualenv .env
 source .env/bin/activate
